3c028ef21e3100e97e7ba6e8d4dd787402557354
RomanNosachev <Walther.landmine@gmail.com>
2017-04-15 19:14:11 +0300

    Architecture edit: added playing sounds of riding and hitting in a tank

---
+++ src/view/renderer/Player.java
@@ -0,0 +1,9 @@
+package view.renderer;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+
+public interface Player {
+    public void play(GameContainer gc) throws SlickException, InterruptedException;
+
+}
---
+++ src/view/tank/TankPlayer.java
@@ -0,0 +1,67 @@
+package view.tank;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Sound;
+
+import model.tank.Move;
+import model.tank.Tank;
+import view.renderer.Player;
+
+public class TankPlayer implements Player {
+    private Tank                playingObject;
+    
+    private Sound               move;
+    private ArrayList<Sound>    hit;
+    private boolean             hitPlaying = false;
+    private int                 hitPlayingIndex = 0;
+
+    public TankPlayer(Tank object)
+    {
+        playingObject = object;
+    }
+    
+    public void setHit(ArrayList<Sound> hit)
+    {
+        this.hit = hit;
+    }
+    
+    public void setMove(Sound move)
+    {
+        this.move = move;
+    }
+
+    @Override
+    public void play(GameContainer gc) throws SlickException
+    {
+        if (playingObject.isMoving() == Move.FORTH || playingObject.isMoving() == Move.BACK)
+        {
+            if (!move.playing())
+                move.play();
+        }
+        else 
+        {            
+            move.stop();
+        }
+                        
+        if (!hit.get(hitPlayingIndex).playing())
+        {
+            hitPlaying = false;
+        }
+        
+        if (playingObject.isDamaged())
+        {                        
+            if (!hitPlaying)
+            {
+                hitPlaying = true;
+                Random rand = new Random();
+                hitPlayingIndex = rand.nextInt(hit.size());
+
+                hit.get(hitPlayingIndex).play();
+            }
+        }
+    }
+}
